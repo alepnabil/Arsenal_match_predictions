28ab0f0c10418cf3e80ed23dfe3c3a1cdca0ad25
alepnabil <aliffni72@gmail.com>
2021-12-04 16:42:13 +0800

    making base class for data scraping

---
+++ main_scrape.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import selenium
+from selenium import webdriver
+import time
+
+class Scrape():
+    pass
+
+
+class Gk():
+    pass
+
+class Def():
+    pass
+
+class Mid():
+    pass
+
+class Attack():
+    pass
