c6161d0b2e1614d35093d4a2f96cce736261e2bc
alepnabil <aliffni72@gmail.com>
2021-12-05 13:51:51 +0800

    added script to get indv football links

--- main_scrape.py
+++ main_scrape.py
@@ -5,9 +5,42 @@ import selenium
 from selenium import webdriver
 import time
 
-class Scrape():
-    pass
 
+
+class Scrape():
+
+    def __init__(self,path):
+        self.path=path
+        self.driver=webdriver.Chrome(self.path)
+    
+    def request_page(self):
+        self.driver.get('https://www.whoscored.com/Teams/13/Fixtures/England-Arsenal')
+        self.soup=BeautifulSoup(self.driver.page_source,'html.parser')
+        time.sleep(3)
+    
+    def match_report_links(self):
+        individual_links=[]
+        print('-----GETTING ALL MATCH LINKS--------')
+        time.sleep(3)
+        try:
+            #this will get all the mathes link. we only want the premier leagues
+            match_links=self.soup.find_all('a',{"class": "horiz-match-link result-1"},href=True)
+            for x in match_links:
+                #will get only the premier league links
+                if 'Premier-League' in x['href']:
+                    #print(x['href'])
+                    #print('---------')
+                    individual_links.append(x['href'])
+                else:
+                    pass
+        except:
+            print('CANNOT GET ALL MATCH LINKS')
+        return individual_links
+
+    def go_to_each_match_report(self):
+        links=self.match_report_links()
+        print(links)
+        
 
 class Gk():
     pass
@@ -20,3 +53,8 @@ class Mid():
 
 class Attack():
     pass
+
+
+scrape=Scrape('C:\\Program Files (x86)\\chromedriver.exe')
+scrape.request_page()
+scrape.go_to_each_match_report()
