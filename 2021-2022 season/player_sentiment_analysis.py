fc2c5c6f61326ead26c23e779a81e32bd6557588
alepnabil <aliffni72@gmail.com>
2022-02-23 17:51:21 +0800

    add file to make sentiment analysis

---
+++ 2021-2022 season/player_sentiment_analysis.py
@@ -0,0 +1,9 @@
+#this script is to get actual players name every match and carry out sentiment analysis on what people have to say on them at twitter
+
+
+#import class from another script just to get the names from the appended list
+from calculate_weekly_performance import player_names
+
+
+for x in player_names:
+    print(x)
