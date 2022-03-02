4c842de6e3cf04aad8e1e5ca744e0b7b34bd1d6e
alepnabil <aliffni72@gmail.com>
2022-03-02 13:08:22 +0800

    add to database

--- 2021-2022 season/player_sentiment_analysis.py
+++ 2021-2022 season/player_sentiment_analysis.py
@@ -1,4 +1,6 @@
 #this script is to get actual players name every match and carry out sentiment analysis on what people have to say on them at twitter
+#this script also calcualte the actual game performance for every post match
+
 
 from calculate_weekly_performance import player_names
 import tweepy as tw
@@ -21,6 +23,8 @@ from nltk.stem import WordNetLemmatizer
 from nltk.stem import LancasterStemmer
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
+
+from sqlalchemy import create_engine 
 class Tweets():
 
     def __init__(self,count,file_path):
@@ -40,6 +44,15 @@ class Tweets():
         self.date_until = str(self.today.strftime("%Y-%m-%d"))
         self.language='en'
         self.count=count
+
+
+        #get api keys
+        self.user=self.config['database']['user']
+        self.password=self.config['database']['password']
+        self.host=self.config['database']['host']
+        self.database=self.config['database']['database']
+        self.engine = create_engine("mysql+pymysql://" + self.user+ ":" + self.password + "@" + self.host + "/" + self.database)
+
 
     def authorize_api(self):
         try:
@@ -65,7 +78,7 @@ class Tweets():
                 #search for tweets mentioning the player
                 tweets = self.api.search_tweets(q=search_words+'-filter:retweets',
                                     until=self.date_until,
-                                    result_type='mixed',
+                                    result_type='recent',
                                     lang=self.language,
                                     count=self.count
                                     )
@@ -83,7 +96,7 @@ class Tweets():
             except :
                 sys.exit("Problem using twitter APIs")
         self.df=pd.DataFrame(df)
-        print(self.df)
+        
 
 
 class Cleaning_tweet(Tweets):
@@ -105,18 +118,12 @@ class Cleaning_tweet(Tweets):
         self.stop=stopwords.words('english')
         self.ls = LancasterStemmer()
 
-        try:
-            self.df['tweet']=self.df['tweet'].apply(lambda x:x.lower())
-            #self.df['tweet']=self.df['tweet'].apply(lambda x:re.sub(r'[\W\s]', ' ', x))
-            self.df['tweet']=self.df['tweet'].apply(lambda x : ' '.join(review for review in x.split() if review not in self.stop))
-            #self. df['tweet']=self.f['tweet'].apply(self.remove_emoji)
-            self.df['tweet']=self.df['tweet'].apply(lambda x: ls.stem(x))
-        except:
-            print('-------ERROR CLEANING TWEET------')
-        
-        print('-----CLEANED DATAFRAME-----')
-        print(self.df)
+        #stemming the tweet
+        self.df['tweet']=self.df['tweet'].apply(lambda x: self.ls.stem(x))
+        self.df['tweet']=self.df['tweet'].apply(lambda tweet:re.sub(r"https?:\/\/\S+","",tweet))
 
+        
+        
 class Sentiment(Cleaning_tweet):
 
     #to categorize the tweets based on their sentiments
@@ -127,8 +134,21 @@ class Sentiment(Cleaning_tweet):
         self.df=pd.concat([self.df,self.sentiment],axis=1)
 
         self.df['sentiment']=self.df['compound'].apply(lambda x: 'positive' if x>=0.05 else ('neutral' if -0.05<x<0.05 else 'negative'))
+        print(self.df)
 
-    
-tweets=Sentiment(int(input("How many tweets per player: ")),r'E:\New folder\Udemy\personal data science projects\Arsenal\2021-2022 season\arsenal_match_preds\2021-2022 season\config.ini')
+
+class Database(Sentiment):
+
+    def update_database(self):
+        try:
+            self.df.to_sql('player_sentiment3',con=self.engine,if_exists='append',index=False)
+            print("Done update to database")
+        except:
+            print("Cannot connect to database")
+
+
+tweets=Database(int(input("How many tweets per player: ")),r'E:\New folder\Udemy\personal data science projects\Arsenal\2021-2022 season\arsenal_match_preds\2021-2022 season\config.ini')
 tweets.get_tweets()
+tweets.clean_tweets()
 tweets.calculate_sentiment()
+tweets.update_database()
